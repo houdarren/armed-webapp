655c4cf44f3f5712530970b0ecbb84caeb0ff159
Xlegic Sin'Austin <42656369+SinAustin@users.noreply.github.com>
2019-09-07 17:41:00 -0500

    add detection class (#2)

--- armed/detection/classes.py
+++ armed/detection/classes.py
@@ -3,33 +3,44 @@ import os
 import six.moves.urllib as urllib
 import sys
 import tarfile
-import tensorflow a tf
+import tensorflow as tf
 import zipfile
 from distutils.version import StrictVersion
-from Collections import defaultdict
+from collections import defaultdict
 from io import StringIO
 sys.path.append("..")
 from object_detection.utils import ops as utils_ops
-from object_detection.utils import label_map_utils
+from object_detection.utils import label_map_util
 from object_detection.utils import visualization_utils as vis_util
 from matplotlib import pyplot as plt
 from PIL import Image
-import CV2
 
 
 class Detector:
+    def __init__(self):
+        pass
 
 
 
-
-
-
+    def _get_detection_graph(self):
+        detection_graph = tf.Graph()
+        with detection_graph.as_default():
+            od_graph_def = tf.GraphDef()
+            # grabbing the inference graph
+            with tf.gfile.GFile('./model_parts/frozen_inference_graph.pb', 'rb') as fid:
+                serialized_graph = fid.read()
+                od_graph_def.ParseFromString(serialized_graph)
+                tf.import_graph_def(od_graph_def, name='')
+        # grabbing the label map
+        category_index = label_map_util.create_category_index_from_labelmap('./model_parts/labelmap.pbtxt', use_display_name=True)
+        return detection_graph, category_index
 
     #Turning the image into an array.
     def _image_to_array(self,image_path):
-        image = Image.open(imagepath)
+        image = Image.open(image_path)
         (img_width, img_height) = image.size
-        return np.array(image.getdata()).reshape(img_height, img_width, 3)).astype(np.uint8)
+
+        return np.array(image.getdata()).reshape(img_height, img_width, 3).astype(np.uint8)
     #run inference graph over a image labeling any identified objects
     def _detect_image(self, image, graph):
       with graph.as_default():
@@ -76,12 +87,15 @@ class Detector:
             output_dict['detection_masks'] = output_dict['detection_masks'][0]
       return output_dict
     #run the detector over a image and saved the output image
-    def _detect(self,image_path,output_pat3h):
+
+    def detect(self,image_path,output_path):
+      image_size = (14,14)
+      detection_graph, category_index = self._get_detection_graph()
 
       image = Image.open(image_path)
-      image_np = _image_to_array(image)
+      image_np = self._image_to_array(image_path)
       image_np_expanded = np.expand_dims(image_np, axis=0)
-      output_dict = _detect_image(image_np_expanded, detection_graph)
+      output_dict = self._detect_image(image_np_expanded, detection_graph)
       vis_util.visualize_boxes_and_labels_on_image_array(
           image_np,
           output_dict['detection_boxes'],
@@ -91,6 +105,6 @@ class Detector:
           instance_masks=output_dict.get('detection_masks'),
           use_normalized_coordinates=True,
           line_thickness=6)
-      plt.figure(figsize=IMAGE_SIZE)
+      plt.figure(figsize=image_size)
       plt.imshow(image_np)
       plt.savefig(output_path)
